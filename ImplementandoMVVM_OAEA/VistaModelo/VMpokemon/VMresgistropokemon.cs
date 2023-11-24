561d209d2161ae7140762ac33271d4a50e9060c1
AlanEA04 <omar.encinas.cetis69@gmail.com>
2023-11-24 12:02:58 -0700

    actividad 5

--- ImplementandoMVVM_OAEA/VistaModelo/VMpokemon/VMresgistropokemon.cs
+++ ImplementandoMVVM_OAEA/VistaModelo/VMpokemon/VMresgistropokemon.cs
@@ -11,38 +11,69 @@ namespace ImplementandoMVVM_OAEA.VistaModelo.VMpokemon
     {
 
         #region VARIABLES
-        string _Texto;
+        string _Txtcolorfondo;
+        string _Txtcolorpoder;
+        string _Txtnombre;
+        string _Txtnro;
+        string _Txtpoder;
+        string _Txticono;
         #endregion
         #region CONTRUCTOR
         public VMresgistropokemon(INavigation navigation)
         {
             Navigation = navigation;
         }
-
         #endregion
         #region OBJETOS
-        public string Texto
+        public string Txtcolorfondo
         {
-            get { return _Texto; }
-            set { SetValue(ref _Texto, value); }
+            get { return _Txtcolorfondo; }
+            set { SetValue(ref _Txtcolorfondo, value); }
         }
+
+        public string Txtcolorpoder
+        {
+            get { return _Txtcolorpoder; }
+            set { SetValue(ref _Txtcolorpoder, value); }
+        }
+
+        public string Txtnombre
+        {
+            get { return _Txtnombre; }
+            set { SetValue(ref _Txtnombre, value); }
+        }
+
+        public string Txtnro
+        {
+            get { return _Txtnro; }
+            set { SetValue(ref _Txtnro, value); }
+        }
+
+        public string Txtpoder
+        {
+            get { return _Txtpoder; }
+            set { SetValue(ref _Txtpoder, value); }
+        }
+
+        public string Txticono
+        {
+            get { return _Txticono; }
+            set { SetValue(ref _Txticono, value); }
+        }
+
         #endregion
         #region PROCESOS
-
         public async Task ProcesoAsyncrono()
         {
 
         }
-
         public void ProcesoSimple()
         {
-
 
         }
         #endregion
         #region COMANDOS
         public ICommand ProcesoAsyncommand => new Command(async () => await ProcesoAsyncrono());
-
         public ICommand ProcesoSimpcommand => new Command(ProcesoSimple);
         #endregion
     }
